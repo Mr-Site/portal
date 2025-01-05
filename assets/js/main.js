80c7e10397f0f8738697a1728bbac2fe39274a61
Mr.Site <matinayoobi00@gmail.com>
2025-01-05 11:46:17 +0330

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,40 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// باز شدن منو همبرگری
+
+function openMenuMobile() {
+  kadrMenuMobile = document.querySelector(".kadrMenuMobile");
+  iconOpen = document.querySelector(".iconOpenMenu");
+  iconClose = document.querySelector(".iconCloseMenu");
+  kadrDisableScroll = document.querySelector(".kadrDisableScroll");
+
+  iconOpen.style.zIndex = "0";
+  iconOpen.style.visibility = "hidden";
+
+  iconClose.style.zIndex = "2";
+  iconClose.style.visibility = "visible";
+
+  kadrDisableScroll.style.visibility = "visible";
+
+  kadrMenuMobile.style.right = "0";
+
+  // غیرفعال کردن اسکرول صفحه
+  document.body.classList.add("no-scroll");
+}
+
+function closeMenuMobile() {
+  kadrMenuMobile.style.right = "-500px";
+
+  iconClose.style.zIndex = "0";
+  iconClose.style.visibility = "hidden";
+
+  iconOpen.style.zIndex = "2";
+  iconOpen.style.visibility = "visible";
+
+  kadrDisableScroll.style.visibility = "hidden";
+
+  // فعال‌سازی دوباره اسکرول صفحه
+  document.body.classList.remove("no-scroll");
+}
